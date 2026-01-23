08649f0612cb1af46124657ba8765f3402d4add0
shaka <sh4k4175@gmail.com>
2026-01-23 07:42:33 +0700

    V2.7

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-container">
         <!-- <a href="update/update.html">List Update</a> -->
-        <a href="partials/update-list.php">V2.5</a>
+        <a href="partials/update-list.php">V2.7</a>
     </div>
 </footer>
